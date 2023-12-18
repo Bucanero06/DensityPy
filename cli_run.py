350d24a52b0983adefe30a9d95bcde4646a1f5b5
ruben <ruben@moonshot.codes>
2023-12-17 23:58:15 -0500

    renamed from cli_v1 and updated inputs to match run_densitypy function

--- cli_run.py
+++ cli_run.py
@@ -2,15 +2,15 @@ import click
 
 
 @click.command()
-@click.argument("json_config_path")
 @click.argument("study_directory")
+@click.argument("json_config_path")
 @click.option("--molcas_input", help="Path to the Molcas input file, this enables molcas execution")
 @click.option("--run_charge_migration", is_flag=True, help="Enable charge migration", default=False)
 @click.option("--run_charge_migration_ft", is_flag=True, help="Enable charge migration FT", default=False)
 @click.option("--run_spectrum_reconstruction", is_flag=True, help="Enable spectrum reconstruction", default=False)
 @click.option("--field_file_help", is_flag=True, help="Enable field file help", default=False)
 @click.option("--molcas_input_help", is_flag=True, help="Enable Molcas input help", default=False)
-@click.option("--lus", is_flag=True, help="Enable LUS", default=False)
+@click.option("--scforbs", is_flag=True, help="Run SCF Orbital Calculations and plot", default=False)
 @click.option("--gridit", is_flag=True, help="Enable grid flag", default=False)
 @click.option("--write_charge_migration", is_flag=True, help="Write charge migration", default=False)
 @click.option("--debug_mode", is_flag=True, help="Enable debug mode", default=False)
@@ -25,9 +25,30 @@ import click
               default="all DEB_FLAG=d")
 @click.option("--plot", is_flag=True, help="Enable plotting", default=False)
 def cli_run(json_config_path, study_directory, molcas_input, run_charge_migration,
-            run_charge_migration_ft, run_spectrum_reconstruction, field_file_help, molcas_input_help, lus, gridit,
+            run_charge_migration_ft, run_spectrum_reconstruction, field_file_help, molcas_input_help, scforbs, gridit,
             write_charge_migration, debug_mode, justh5, justgetdipoles, justgetdensity, weights_file, givenfieldfile,
             make, make_directory, make_flags, plot):
+    """
+     Entry Way to DensityPy
+    This CLI is engineered to facilitate computational chemistry simulations with OpenMolcas and the ASTRA-ChargeMigration Fortran code.
+
+    Functionality and Scope:
+        Molcas Integration: Incorporates Molcas for quantum chemical calculations, essential for accurate modeling of electronic structures.
+        Charge Migration Analysis: Executes both standard and Fourier-transformed charge migration simulations, offering deep insights into molecular dynamics.
+        Data Visualization and Analysis: Provides tools for generating plots and graphs, crucial for interpreting complex simulation data.
+        Configurable Workflow: Leverages a JSON-based configuration system, allowing for flexible and detailed setup of simulation parameters.
+
+    Parameters
+        json_config_path: String, path to the JSON configuration file.
+        study_directory: String, directory path for output and intermediate files.
+        molcas_input: String, optional path to the Molcas input file.
+        Additional flags and options to control specific features and modes of operation.
+
+    Usage:
+        python cli_v1.py configuration_help.json /home/ruben/PycharmProjects/DensityPy/Studies/cluttertest/ --make --make_directory=densityfort --make_flags='all' --run_charge_migration --run_charge_migration_ft --run_spectrum_reconstruction --plot
+
+    """
+
     from densitypy.main import run_densitypy
     run_densitypy(
         json_config_path=json_config_path,
@@ -39,7 +60,7 @@ def cli_run(json_config_path, study_directory, molcas_input, run_charge_migratio
         plot=plot,
         #
         field_file_help=field_file_help, molcas_input_help=molcas_input_help,
-        lus=lus, gridit=gridit, write_charge_migration=write_charge_migration, debug_mode=debug_mode,
+        scforbs=scforbs, gridit=gridit, write_charge_migration=write_charge_migration, debug_mode=debug_mode,
         justh5=justh5, justgetdipoles=justgetdipoles, justgetdensity=justgetdensity, weights_file=weights_file,
         givenfieldfile=givenfieldfile,
         make_fortran=make,
